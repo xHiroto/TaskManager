84d858b799223fd70820788a41058690788cc80a
19036308 <19036308@myrp.edu.sg>
2021-07-29 12:28:45 +0800

    Done DBHelper

---
+++ app/src/main/java/sg/edu/rp/c346/id19045784/taskmanager/DBHelper.java
@@ -0,0 +1,86 @@
+package sg.edu.rp.c346.id19045784.taskmanager;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    // increment by 1 whenever db schema changes.
+    private static final int DATABASE_VER = 1;
+    // Filename of the database
+    private static final String DATABASE_NAME = "tasks.db";
+    private static final String TABLE_TASK = "task";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_DESCRIPTION = "description";
+    private static final String COLUMN_NAME = "name";
+
+    public DBHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableSql = "CREATE TABLE " + TABLE_TASK +  "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NAME + " TEXT,"
+                + COLUMN_DESCRIPTION + " TEXT )";
+        db.execSQL(createTableSql);
+        Log.i("info" ,"created tables");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
+        // Create table(s) again
+        onCreate(db);
+    }
+
+    public void insertTask(String description, String name){
+
+        // Get an instance of the database for writing
+        SQLiteDatabase db = this.getWritableDatabase();
+        // We use ContentValues object to store the values for
+        //  the db operation
+        ContentValues values = new ContentValues();
+        // Store the column name as key and the name as value
+        values.put(COLUMN_NAME, name);
+        // Store the column name as key and the description as value
+        values.put(COLUMN_DESCRIPTION, description);
+        // Insert the row into the TABLE_TASK
+        db.insert(TABLE_TASK, null, values);
+        // Close the database connection
+        db.close();
+    }
+
+    public ArrayList<String> getTasks() {
+        ArrayList<String> tasks = new ArrayList<String>();
+        String selectQuery = "SELECT " + COLUMN_ID + ", "
+                + COLUMN_NAME + ", "
+                + COLUMN_DESCRIPTION
+                + " FROM " + TABLE_TASK;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(0);
+                String name = cursor.getString(1);
+                String description = cursor.getString(2);
+//                Task obj = new Task(id, description, date);
+                tasks.add(name + "\n" + description);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return tasks;
+    }
+}
