84c699932abbcac1f4dff1c70716f9f16f6b4098
gab <19020303@myrp.edu.sg>
2021-07-29 14:33:30 +0800

    Notification done

--- app/src/main/java/sg/edu/rp/c346/id19045784/taskmanager/AddActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19045784/taskmanager/AddActivity.java
@@ -2,13 +2,20 @@ package sg.edu.rp.c346.id19045784.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.Calendar;
+
 public class AddActivity extends AppCompatActivity {
+
+    int reqCode = 12345;
     EditText etName, etDesc;
     Button btnAdd, btnCancel;
 
@@ -25,6 +32,25 @@ public class AddActivity extends AppCompatActivity {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Calendar calendar = Calendar.getInstance();
+                calendar.add(Calendar.SECOND, 5);
+
+                Intent intent = new Intent(
+                        AddActivity.this,
+                        NotificationReceiver.class);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                        AddActivity.this,
+                        reqCode,
+                        intent,
+                        PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager)
+                        getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),pendingIntent);
+
+
                 String name  = etName.getText().toString();
                 String desc = etDesc.getText().toString();
 
