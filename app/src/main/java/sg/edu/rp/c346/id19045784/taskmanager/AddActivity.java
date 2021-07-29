d047d29e4a702ac78f2199c2bf1b71a5ece82d89
Ahmed <19008424@myrp.edu.sg>
2021-07-29 13:44:37 +0800

    Done Main activity

--- app/src/main/java/sg/edu/rp/c346/id19045784/taskmanager/AddActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19045784/taskmanager/AddActivity.java
@@ -2,6 +2,7 @@ package sg.edu.rp.c346.id19045784.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,7 +31,8 @@ public class AddActivity extends AppCompatActivity {
                 DBHelper dbh = new DBHelper(AddActivity.this);
                 dbh.insertTask(desc, name);
                 dbh.close();
-                finish();
+                Intent i = new Intent(AddActivity.this,MainActivity.class);
+                startActivity(i);
 
             }
         });
--- app/src/main/java/sg/edu/rp/c346/id19045784/taskmanager/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id19045784/taskmanager/MainActivity.java
@@ -2,13 +2,49 @@ package sg.edu.rp.c346.id19045784.taskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnTask;
+    ListView lv;
+    ArrayList<String> altasks;
+    ArrayAdapter<String> aaTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final DBHelper dbTask = new DBHelper(MainActivity.this);
+        btnTask = findViewById(R.id.btnTask);
+        lv = findViewById(R.id.lv);
+        altasks = dbTask.getTasks();
+        aaTasks = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,altasks);
+
+
+        aaTasks.notifyDataSetChanged();
+
+        lv.setAdapter(aaTasks);
+
+
+
+        btnTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,AddActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
     }
+
 }
